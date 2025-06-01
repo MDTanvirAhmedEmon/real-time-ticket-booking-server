1dea451953eeeecefef37cfa829eb9c49ed96e04
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-06-01 10:53:34 +0600

    updated validation

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod';
 
 const loginValidationSchema = z.object({
-  email: z.string({ required_error: 'Email is required.' }),
-  password: z.string({ required_error: 'Password is required' }),
+    email: z.string({ required_error: 'Email is required.' }),
+    password: z.string({ required_error: 'Password is required' }),
 });
+
 
 const changePasswordValidationSchema = z.object({
   body: z.object({
